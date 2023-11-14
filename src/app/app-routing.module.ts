487804fce6cb526731d2356dd24368d2dd03260d
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-14 10:42:20 -0300

    implementado auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// Guard
+import { AuthGuard } from './core/guard/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
@@ -9,7 +12,7 @@ const routes: Routes = [
   },
   {
     path: 'admin',
-    // canActivateChild: [AuthGuard],
+    canActivateChild: [AuthGuard],
     loadChildren: () =>
       import('./components/admin/admin.module').then((m) => m.AdminModule),
   },
